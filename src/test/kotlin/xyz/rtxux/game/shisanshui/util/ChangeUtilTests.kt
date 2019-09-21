168842d10e3a7a666333dbed5bf19662352bb231
jihuayu <8042833@qq.com>
2019-09-21 15:58:26 +0800

    add test

---
+++ src/test/kotlin/xyz/rtxux/game/shisanshui/util/ChangeUtilTests.kt
@@ -0,0 +1,52 @@
+package xyz.rtxux.game.shisanshui.util
+
+import org.junit.Test
+import xyz.rtxux.game.shisanshui.model.Card
+
+class ChangeUtilTests {
+    @Test fun testStrToPoint() {
+        assert(strToPoint("2")==1)
+        assert(strToPoint("3")==2)
+        assert(strToPoint("4")==3)
+        assert(strToPoint("5")==4)
+        assert(strToPoint("6")==5)
+        assert(strToPoint("7")==6)
+        assert(strToPoint("8")==7)
+        assert(strToPoint("9")==8)
+        assert(strToPoint("1")==9)
+        assert(strToPoint("0")==9)
+        assert(strToPoint("10")==9)
+        assert(strToPoint("J")==10)
+        assert(strToPoint("j")==10)
+        assert(strToPoint("Q")==11)
+        assert(strToPoint("q")==11)
+        assert(strToPoint("k")==12)
+        assert(strToPoint("K")==12)
+        assert(strToPoint("A")==13)
+        assert(strToPoint("a")==13)
+    }
+
+    @Test fun testCardsToStr() {
+        assert(cardsToStr(listOf("*1","$2","*3")) == "*1$2*3")
+        assert(cardsToStr(listOf("*1")) == "*1")
+        assert(cardsToStr(listOf("*1","*2","*3")) == "*1*2*3")
+        assert(cardsToStr(listOf("*1","*A","*3")) == "*1*A*3")
+    }
+
+    @Test fun testStrToCards() {
+        assert((listOf(Card(9,3),Card(1,1),Card(2,3)) == strToCards("*1$2*3")))
+        assert((listOf(Card(9,3),Card(1,1),Card(2,3)) == strToCards("*10$2*3")))
+        assert((listOf(Card(9,3),Card(1,1),Card(2,3)) == strToCards("*1$2*3")))
+    }
+
+    @Test fun testCardToPoint() {
+        assert(cardToPoint("*2") == 1)
+        assert(cardToPoint("#2") == 1)
+        assert(cardToPoint("*10") == 9)
+        assert(cardToPoint("&2") == 1)
+        assert(cardToPoint("$2") == 1)
+        assert(cardToPoint("*A") == 13)
+        assert(cardToPoint("*J") == 10)
+
+    }
+}
---
+++ src/test/kotlin/xyz/rtxux/game/shisanshui/util/SpTypeUtilTests.kt
@@ -0,0 +1,34 @@
+package xyz.rtxux.game.shisanshui.util
+
+import org.junit.Test
+
+class SpTypeUtilTests {
+    @Test fun testZhiZunQinLong(){
+        assert(1== zhiZunQinLong(strToCards("*2*3*4*5*6*7*8*9*10*j*q*k*a")))
+        assert(1== zhiZunQinLong(strToCards("*2*3*4*5*6*7*8*9*10*j*q*k*a")))
+        assert(0== zhiZunQinLong(strToCards("*2*3*4*6*6*7*8*9*10*j*q*k*a")))
+        assert(0== zhiZunQinLong(strToCards("#2*3*4*6*6*7*8*9*10*j*q*k*a")))
+        assert(0== zhiZunQinLong(strToCards("#2*3*4*5*6*7*8*9*10*j*q*k*a")))
+        assert(0== zhiZunQinLong(strToCards("#2*3*4*5*6*8*8*9*10\$j*q*k*a")))
+    }
+
+    @Test fun testYiTiaoLong(){
+        assert(1== yiTiaoLong(strToCards("*2*3*4*5*6*7*8*9*10*j*q*k*a")))
+        assert(1== yiTiaoLong(strToCards("*2*3*4*5*6*7*8*9*10*j*q*k*a")))
+        assert(0== yiTiaoLong(strToCards("*2*3*4*6*6*7*8*9*10*j*q*k*a")))
+        assert(0== yiTiaoLong(strToCards("#2*3*4*6*6*7*8*9*10*j*q*k*a")))
+        assert(1== yiTiaoLong(strToCards("*2$3*4*5*6*7*8*9*10*j*q*k*a")))
+        assert(0== yiTiaoLong(strToCards("#2*3*4*5*6*8*8*9*10\$j*q*k*a")))
+    }
+
+    @Test fun testShiErHuangZhu(){
+        assert(1== shiErHuangZhu(strToCards("*q*k*k*k*a*a*a*q*q*j*q*k*a")))
+        assert(0== shiErHuangZhu(strToCards("*1*1*1*1*j*j*j*q*q*j*q*k*a")))
+        assert(0== shiErHuangZhu(strToCards("*1*1*1*1*j*j*j*q&q*j*q*k*a")))
+        assert(0== shiErHuangZhu(strToCards("*1*1*1*1*j*j*j*q\$q*j*q*k*a")))
+        assert(0== shiErHuangZhu(strToCards("*1*1*1*1*j*j*j*q#q*j*q*k*a")))
+        assert(0== shiErHuangZhu(strToCards("*1*1*1*1*j*j*j*q#q*j*q*k*a")))
+        assert(0== shiErHuangZhu(strToCards("*1*1*1*9*j*j*j*q#q*j*q*k*a")))
+
+    }
+}
